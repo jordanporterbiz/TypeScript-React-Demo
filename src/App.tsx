f2e41baecfbb0ecc413d48964e0029236aee0792
Jordan Porter <53372392+JordanPorterDev@users.noreply.github.com>
2021-11-27 22:13:28 +0000

    useContext hook with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ import { Oscar } from './components/Oscar'
 import { Button } from './components/Button'
 import { Input } from './components/Input'
 import { Container } from './components/Container'
+import { Counter } from './components/state/Counter'
+import { Box } from './components/context/Box'
+import { ThemeContextProvider } from './components/context/ThemeContext'
+import { User } from './components/context/User'
+import { UserContextProvider } from './components/context/UserContext'
 
 function App() {
 
@@ -38,18 +43,48 @@ function App() {
 
   return (
     <div className="App">
-      <Greet name='Jordan' isLoggedIn={true} levelCount={200}/> 
+      <br/>
+      <br/>
+      <Greet name='Jordan' isLoggedIn={true} levelCount={200}/>
+      <br/>
+      <br/>
       <Person name={personName}/>
+      <br/>
+      <br/>
       <PersonList list={nameList}/>
+      <br/>
+      <br/>
       <Status status='success'/>
+      <br/>
+      <br/>
       <Oscar>
         <Heading>Oscar goes to PORTER!!! :D :D </Heading>
       </Oscar>
+      <br/>
+      <br/>
       <Button handleClick={(event, id) => {
         console.log('Button has been clicked!!', event, id)
       }}/>
+      <br/>
+      <br/>
       <Input value='' handleChange={event => console.log(event)}/>
+      <br/>
+      <br/>
       <Container styles={{ border: '1px solid black', padding: '1rem'}} />
+      <br/>
+      <br/>
+      <Counter/>
+      <br/>
+      <br/>
+      <ThemeContextProvider>
+        <Box/>
+      </ThemeContextProvider>
+      {/* With Future Context values */}
+      <UserContextProvider>
+        <User/>
+      </UserContextProvider>
+
+
     </div>
   );
 }
