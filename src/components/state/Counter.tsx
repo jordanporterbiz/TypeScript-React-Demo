7f5469c9bb7de72f11f1d5ed5715929bdac2aee5
Jordan Porter <53372392+JordanPorterDev@users.noreply.github.com>
2021-11-27 21:23:13 +0000

    useReducer hook with TypeScript - working with discriminated union types

---
+++ src/components/state/Counter.tsx
@@ -0,0 +1,52 @@
+import { useReducer } from "react";
+
+type CounterState = {
+    count: number
+}
+
+type UpdateAction = {
+    type: 'increment' | 'decrement'
+    payload: number
+}
+
+type ResetAction = {
+    type: 'reset'
+}
+
+type CounterAction = UpdateAction | ResetAction
+
+
+// To enforce different types you can code the above type
+// type CounterAction = {
+//     type: 'increment' | 'decrement' | 'reset'
+//     payload?: number
+// }
+
+const initialState = { count: 0 }
+
+function reducer(state: CounterState, action: CounterAction) {
+    switch(action.type) {
+        case 'increment': 
+            return { count: state.count + action.payload }
+        case 'decrement':
+            return { count: state.count - action.payload }
+        case 'reset':
+            return initialState
+        default: 
+            return state
+    }
+}
+
+export const Counter = () => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    return (
+        <>
+        Count: {state.count}
+        <button onClick={() => dispatch({ type: 'increment', payload: 20})}> Incrememnt 20</button>
+        <button onClick={() => dispatch({ type: 'decrement', payload: 20})}> Decrement 20</button>
+        <button onClick={() => dispatch({ type: 'reset',})}>Reset</button>
+
+        </>
+    )
+}
