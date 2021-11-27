b6c790c664b55cb70be8528c4c829ae2bcf1b4cd
Jordan Porter <53372392+JordanPorterDev@users.noreply.github.com>
2021-11-27 16:32:13 +0000

    handling event props in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { PersonList } from './components/PersonList'
 import { Status } from './components/Status'
 import { Heading } from './components/Heading'
 import { Oscar } from './components/Oscar'
+import { Button } from './components/Button'
+import { Input } from './components/Input'
 
 
 function App() {
@@ -43,6 +45,10 @@ function App() {
       <Oscar>
         <Heading>Oscar goes to PORTER!!! :D :D </Heading>
       </Oscar>
+      <Button handleClick={(event, id) => {
+        console.log('Button has been clicked!!', event, id)
+      }}/>
+      <Input value='' handleChange={event => console.log(event)}/>
     </div>
   );
 }
